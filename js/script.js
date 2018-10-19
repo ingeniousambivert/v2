802b4fd93b209b5d792594c6d4699167a8d9ec19
Monarch Maisuriya <37824914+ingeniousambivert@users.noreply.github.com>
2018-10-19 11:12:53 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -31,11 +31,28 @@ window.onscroll = function () {
 
 
 function navText() {
-    hm.style.color = "black";
-    ab.style.color = "black";
-    cn.style.color = "black";
-    hm.style.borderColor = "black";
-    ab.style.borderColor = "black";
-    cn.style.borderColor = "black";
+    if (window.pageYOffset > 51) {
+        hm.style.color = "black";
+        ab.style.color = "black";
+        cn.style.color = "black";
+        hm.style.borderColor = "black";
+        ab.style.borderColor = "black";
+        cn.style.borderColor = "black";
+    }
+    if (window.screen) {
+        var width = screen.width;
+        var height = screen.height;
 
+        if (width < 400 && height < 600) {
+            hm.style.color = "white";
+            ab.style.color = "white";
+            cn.style.color = "white";
+            hm.style.borderColor = "white";
+            ab.style.borderColor = "white";
+            cn.style.borderColor = "white";
+
+        }
+
+
+    }
 }
