988b74c37c3fd28a5d954a608decd6da0a435d87
Monarch Maisuriya <37824914+ingeniousambivert@users.noreply.github.com>
2018-10-19 15:40:22 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -58,3 +58,26 @@ function navTextSR() {
     }
 
     */
+
+// To generate random quotes
+
+function randomQ() {
+    var q = new Array()
+
+    q[1] = "We have seen that computer programming is an art, because it applies accumulated knowledge to the world, because it requires skill and ingenuity, and especially because it produces objects of beauty. - Paul Graham";
+    q[2] = "Programming today is a race between software engineers striving to build bigger and better idiot-proof programs, and the Universe trying to produce bigger and better idiots. So far, the Universe is winning. - Rich Cook";
+    q[3] = "A good programmer is someone who always looks both ways before crossing a one-way street. - Keith Weinberg";
+    q[4] = "Computer science education cannot make anybody an expert programmer any more than studying brushes and pigment can make somebody an expert painter. - Eric S. Raymond ";
+    q[5] = "Most good programmers do programming not because they expect to get paid or get adulation by the public, but because it is fun to code. - Linus Torvalds";
+    q[6] = "You might not think programmers are artists, but programming is extremely creative profession. Its logic-based creativity. - John Romero";
+    q[7] = "Everyone should learn how to program a computer because it teaches you how to think. - Steve Jobs";
+    q[8] = "First solve the problem , then write the code. - John Johnson";
+    q[9] = "The most important property of a program is whether it accomplishes the intention of its user. - C.A.R Hoare";
+    q[10] = "Truth can only be found in one place: the code. - Robert C. Martin";
+    var ry = Math.floor(Math.random() * q.length);
+    if (ry == 0)
+        ry = 1;
+    document.getElementById("quote").innerHTML = q[ry];
+
+}
+randomQ();
