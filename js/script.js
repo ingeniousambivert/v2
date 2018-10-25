db5b111fff2136c4feec29abf84bc3c80dc6c325
Monarch Maisuriya <37824914+ingeniousambivert@users.noreply.github.com>
2018-10-25 10:35:40 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -92,17 +92,35 @@ randomQ();
 
 
 // Bootstrap Charts
-//doughnut
-var ctxD = document.getElementById("doughnutChart").getContext('2d');
-var myLineChart = new Chart(ctxD, {
-    type: 'doughnut',
+//line
+var ctxL = document.getElementById("lineChart").getContext('2d');
+var myLineChart = new Chart(ctxL, {
+    type: 'line',
     data: {
-        labels: ["HTML5 & CSS3", "Javascript", "PHP & MySQL", "C", "C++", "Java", "Python"],
+        labels: ["January", "February", "March", "April", "May", "June", "July"],
         datasets: [{
-            data: [300, 50, 100, 40, 120, 100, 20],
-            backgroundColor: ["rgba(255, 99, 132,1)", "rgba(255, 159, 64,1)", "rgba(255, 205, 86,1)", "rgba(75, 192, 192,1)", "rgba(54, 162, 235,1)", "rgba(153, 102, 255, 1)", "rgba(243, 131, 113,1)"],
-            hoverBackgroundColor: ["#FF5A5E", "#5AD3D1", "#FFC870", "#A8B3C5", "#616774"]
-        }]
+                label: "My First dataset",
+                data: [65, 59, 80, 81, 56, 55, 40],
+                backgroundColor: [
+                    'rgba(105, 0, 132, .2)',
+                ],
+                borderColor: [
+                    'rgba(200, 99, 132, .7)',
+                ],
+                borderWidth: 2
+            },
+            {
+                label: "My Second dataset",
+                data: [28, 48, 40, 19, 86, 27, 90],
+                backgroundColor: [
+                    'rgba(0, 137, 132, .2)',
+                ],
+                borderColor: [
+                    'rgba(0, 10, 130, .7)',
+                ],
+                borderWidth: 2
+            }
+        ]
     },
     options: {
         responsive: true
